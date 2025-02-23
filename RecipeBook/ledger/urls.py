3ce42fa93fd3b083a4e1be47f4a7bba726827b23
JPChurros <151139142+JPChurros@users.noreply.github.com>
2025-02-23 21:12:24 +0800

    [Recipes] created base file for Recipe 1 and 2
    TODO: Recipe 1 and 2 contents
    
    Signed-off-by: JPChurros <151139142+JPChurros@users.noreply.github.com>

--- RecipeBook/ledger/urls.py
+++ RecipeBook/ledger/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import recipe_list
+from .views import recipe_list, recipeListTemplate
 
 urlpatterns = {
     path('recipes/list/', recipe_list, name='recipe_list'),
+    path('recipe/<int:num>/', recipeListTemplate, name='recipeListTemplate')
 }
 
 app_name = "ledger"
--- RecipeBook/ledger/views.py
+++ RecipeBook/ledger/views.py
@@ -5,4 +5,13 @@ from django.template import loader
 def recipe_list(request):
     template = loader.get_template('homepage.html')
     return HttpResponse(template.render())
+
+def recipeListTemplate(request, num=1):
+    if num == 1:
+        number = "1"
+    elif num == 2:
+        number = "2"
+    else:
+        number = "0"
+    return render(request, 'recipeListTemplate.html', {'number':number})
 # Create your views here.
